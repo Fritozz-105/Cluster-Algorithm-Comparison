2f1efcb96ad686997221baf0b182c08844aae937
Fritozz-105 <zengz1@ufl.edu>
2024-11-27 16:56:27 -0500

    Created some file organization and initial components

---
+++ ClusterAlgorithmComparison/frontend/src/components/Home.tsx
@@ -0,0 +1,11 @@
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home-title">
+        <h1>K-Means Clustering vs Gaussian Mixture Models</h1>
+    </div>
+  );
+};
+
+export default Home;
